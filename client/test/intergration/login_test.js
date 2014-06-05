7ea0f1912dd89c0ee97376392e39596bc1e88571
Andrew McDowell <madoliole@gmail.com>
2014-06-05 22:46:48 +1000

    test(login_test): update tests to remove chaining
    - using andThen removes the need for chaining
    - Added set up and tear down functions
    - Updated the title in the testRunner

--- client/test/intergration/login_test.js
+++ client/test/intergration/login_test.js
@@ -5,21 +5,51 @@
  * Time: 20:48
  * To change this template use File | Settings | File Templates.
  */
-module('Login page', function() {});
 
+// Run before each test case.
+QUnit.testStart(function () {
+    Ember.run(function () { App.reset(); });
+    // Display an error if asynchronous operations are queued outside of
+    // Ember.run.  You need this if you want to stay sane.
+    Ember.testing = true;
+});
+
+// Run after each test case.
+QUnit.testDone(function () {
+    Ember.testing = false;
+});
+
+ 
+module('Login Page', {
+  setup: function(){
+  Ember.run(this, function () {
+      //
+   });
+  }
+});
 test('Display welcome message', function() {
-    visit('/').find('h1').then(function(el) {
+    expect(1);
+    visit('/');
+    andThen(function(){
+        var el = find('h1');
         equal(el.length, 1, 'H1 tag must be present');
     });
 });
 
 
 test('Should display links for Ember Digest, Articles, Photos and Login', function() {
-    visit('/').find('a').then(function(el) {
-        equal(el[0].innerText, 'Ember Digest', 'Ember Digest link must be present');
-        equal(el[1].innerText, 'Articles', 'Articles link must be present');
-        equal(el[2].innerText, 'Photos', 'Photos link must be present');
-        equal(el[3].innerText, 'Login', 'Login link must be present');
-    })
-})
+  expect(4);
+  visit('/');
+  andThen(function() {
+    var el = find('a');
+    equal(el[0].innerText, 'Ember Digest', 'Ember Digest link must be present');
+    equal(el[1].innerText, 'Articles', 'Articles link must be present');
+    equal(el[2].innerText, 'Photos', 'Photos link must be present');
+    equal(el[3].innerText, 'Login', 'Login link must be present');
+   
+  });
+});
+
+
+   
 
