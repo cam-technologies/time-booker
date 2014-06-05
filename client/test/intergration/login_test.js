2c4309ff5d7cfe41b077a366ef2d1dfd9be93839
Andrew McDowell <madoliole@gmail.com>
2014-06-06 00:33:16 +1000

    test(login_test): more tests
    - added tests
    - tried implementing a fake http post using httpRespond but needs more
    thinking, commented out tests that rely on this

--- client/test/intergration/login_test.js
+++ client/test/intergration/login_test.js
@@ -36,4 +36,56 @@ test('Should display links for Ember Digest, Articles, Photos and Login', functi
   });
 });
 
+test('Should go to login route if login link is clicked', function() {
+  expect(1);
+  visit('/');
+  Ember.run(function() {
+        click('a:contains("Login")');
+    });
+  andThen(function() {
+    equal(currentRouteName(), 'login');
+  });
+});
+
+test('Should check the login form exists', function() {
+  expect(3);
+  visit('/login');
+  andThen(function() {
+    var username = find('#username')[0];
+    var password = find('#password')[0];
+    var button = find('#loginSubmit')[0];
+    equal(username.placeholder, 'Username');
+    equal(password.placeholder, 'Password');
+    equal(button.value, 'Log In');
+  });
+});
+
+//@TODO: 
+//  work out how to incorportate https://github.com/trek/ember-testing-httpRespond 
+//  into our code so we can do httpRespond like below and fake the http post
+
+// test('Should fill in login form with correct credentials and click submit', function() {
+//   visit('/login');
+//   fillIn('#username', 'ember');
+//   fillIn('#password', 'casts');
+//   Ember.run(function() {
+//     click('#loginSubmit');
+//     httpRespond('post', '/auth.json', { success: true, token:'ABCDEFGHIJKLMNOPQRSTUVWXYZ'});
+//   });
+//      andThen(function() {
+//     equal(currentRouteName(), 'articles');
+//   });
+// });
+
+// test('Should fill in login form with wrong credentials and click submit', function() {
+//   visit('/login');
+//   fillIn('#username', 'Andrew');
+//   fillIn('#password', 'p4$$w0rd');
+//   click('#loginSubmit');
+
+//   andThen(function() {
+//     equal(currentRouteName(), 'login');
+//   })
+// });
+   
 
