2f0fbd73d5fe420a16ce21958f20bcb2ac7a5890
Andrew McDowell <madoliole@gmail.com>
2014-06-07 13:00:55 +1000

    test(login_page): adding ic-ajax
    - adding ic-ajax to wrap out ajax requests in src code
    - adding ic-ajax default fixtures so we can mock api calls
    - testing login in and successful routing
    - adding ic-ajax to bower

--- client/src/app/controllers/loginController.js
+++ client/src/app/controllers/loginController.js
@@ -1,41 +1,45 @@
 App.LoginController = Ember.Controller.extend({
-    actions: {
-        login: function () {
-            var self = this,
-                data = this.getProperties('username', 'password');
-            // Clear out any error messages.
-            this.set('errorMessage', null);
+  actions: {
+    login: function() {
+      var self = this,
+        data = this.getProperties('username', 'password');
+      // Clear out any error messages.
+      this.set('errorMessage', null);
 
-            $.post('/auth.json', data).then(function (response) {
+      ajax.request({
+        url: '/auth.json',
+        type: 'POST',
+        data: data
+      }).then(function(response) {
 
-                self.set('errorMessage', response.message);
-                if (response.success) {
-                    alert('Login succeeded!');
-                    self.set('token', response.token);
+        self.set('errorMessage', response.message);
+        if (response.success) {
+          alert('Login succeeded!');
+          self.set('token', response.token);
 
-                    var attemptedTransition = self.get('attemptedTransition');
-                    if (attemptedTransition) {
-                        attemptedTransition.retry();
-                        self.set('attemptedTransition', null);
-                    }
-                    else {
-                        // Redirect to 'articles' by default.
-                        self.transitionToRoute('articles');
-                    }
-                }
-            });
+          var attemptedTransition = self.get('attemptedTransition');
+          if (attemptedTransition) {
+            attemptedTransition.retry();
+            self.set('attemptedTransition', null);
+          }
+          else {
+            // Redirect to 'articles' by default.
+            self.transitionToRoute('articles');
+          }
         }
-    },
-    reset: function () {
-        this.setProperties({
-            username: '',
-            password: '',
-            errorMessage: ''
-        });
-    },
+      });
+    }
+  },
+  reset: function() {
+    this.setProperties({
+      username: '',
+      password: '',
+      errorMessage: ''
+    });
+  },
 
-    token: localStorage.token,
-    tokenChanged: function () {
-        localStorage.token = this.get('token');
-    }.observes('token')
+  token: localStorage.token,
+  tokenChanged: function() {
+    localStorage.token = this.get('token');
+  }.observes('token')
 });
